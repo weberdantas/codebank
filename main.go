541ca1409598f5e98cbcb2a02e2bdf3e25e7d240
Weber <weber@MacBook-Pro-de-Weber.local>
2022-08-17 20:25:20 -0300

    Finalização da aplicação

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/weberdantas/codebank/infrastructure/grpc/server"
+	"github.com/weberdantas/codebank/infrastructure/kafka"
+	"github.com/weberdantas/codebank/infrastructure/repository"
+	"github.com/weberdantas/codebank/usecase"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
+
+func main() {
+	db := setupDb()
+	defer db.Close()
+
+	producer := setupKafkaProducer()
+	processTransactionUseCase := setupTransactionUseCase(db, producer)
+	serveGrpc(processTransactionUseCase)
+}
+
+func setupTransactionUseCase(db *sql.DB, producer kafka.KafkaProducer) usecase.UseCaseTransaction {
+	transactionReposity := repository.NewTransactionRepositoryDb(db)
+	useCase := usecase.NewUseCaseTransaction(transactionReposity)
+	useCase.KafkaProducer = producer
+
+	return useCase
+}
+
+func setupKafkaProducer() kafka.KafkaProducer {
+	producer := kafka.NewKafkaProducer()
+	producer.SetupProducer(os.Getenv("KafkaBootstrapServers"))
+
+	return producer
+}
+
+func serveGrpc(processTransactionUseCase usecase.UseCaseTransaction) {
+	grpcServer := server.NewGRPCServer()
+	grpcServer.ProcessTransactionUseCase = processTransactionUseCase
+	fmt.Println("Rodando gRPC Server")
+	grpcServer.Serve()
+}
+
+func setupDb() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("host"),
+		os.Getenv("port"),
+		os.Getenv("user"),
+		os.Getenv("password"),
+		os.Getenv("dbname"),
+	)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal("error connection to database")
+	}
+
+	return db
+}
+
+// Evans - gRPC cliente
+// Comando: evans -r repl -p=50052
